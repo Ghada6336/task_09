e58d7f4ea029df483d11fb91ec3ed0289802a234
Ghada6336 <ghadafahmie6336@gmail.com>
2020-02-17 23:18:35 +0200

    task9

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from .models import Restaurant
+from django.contrib.auth.models import User
 
 class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
         fields = '__all__'
 
+
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username' ,'password']
+
         widgets = {
-        	'opening_time': forms.TimeInput(attrs={'type':'time'}),
-        	'closing_time': forms.TimeInput(attrs={'type':'time'}),
+        'password': forms.PasswordInput(),
         }
+
+class SigninForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
